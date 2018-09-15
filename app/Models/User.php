2f67d5dec311a362780108345971b3f6f98382b2
Ahmad Anbar <aanbar@gmail.com>
2018-09-15 18:36:21 +0300

    Fix full name attribute in template.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,4 +9,9 @@ class User extends Model
 
     protected $fillable = ['first_name', 'last_name', 'email', 'password'];
 
+
+    public function getFullnameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
